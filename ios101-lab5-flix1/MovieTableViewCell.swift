9cf2e2471bc7b8a49b754da9c3078eb1f18d272b
Jaylen Smith <jaylensmith4521@gmail.com>
2024-03-22 03:16:27 -0400

    Movie Table Cell View

---
+++ ios101-lab5-flix1/MovieTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieTableViewCell.swift
+//  ios101-lab5-flix1
+//
+//  Created by Jaylen Smith on 3/21/24.
+//
+
+import UIKit
+import Nuke
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDescription: UILabel!
+    
+    var movie: Movie!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
